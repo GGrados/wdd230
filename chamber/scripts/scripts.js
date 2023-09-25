3c802d7db1a98c26e2e068b8aab0322301c14fbf
GGrados <cesarg.grados@gmail.com>
2023-09-25 18:50:15 -0500

    week 02 final

--- chamber/scripts/scripts.js
+++ chamber/scripts/scripts.js
@@ -1,3 +1,15 @@
+const todaysDate = new Date();
+document.getElementById("year").textContent = todaysDate.getFullYear();
+
+const options = {
+  weekDay: "long",
+  day: "numeric",
+  month: "long",
+  year: "numeric",
+};
+
+document.getElementById("lastModified").textContent = document.lastModified;
+
 const button = document.querySelector("#menu");
 const navList = document.querySelector("nav");
 button.addEventListener("click", () => {
