9abb018144b93675fd3c5b8f1dd0336129cec576
GGrados <cesarg.grados@gmail.com>
2023-10-14 08:20:14 -0500

    Links

---
+++ scripts/links.js
@@ -0,0 +1,2 @@
+const baseURL = "https://github.com/GGrados/wdd230";
+const linksURL = "https://github.com/GGrados/wdd230/data/links.json";
--- scripts/scripts.js
+++ scripts/scripts.js
@@ -52,3 +52,35 @@ if (numVisits !== 0) {
 numVisits++;
 
 localStorage.setItem("numVisits-ls", numVisits);
+
+// Display Current Weather Data
+
+const currentTemp = document.querySelector("#current-temp");
+const weatherIcon = document.querySelector("#weather-icon");
+const captionDesc = document.querySelector("figcaption");
+
+const url =
+  "https://api.openweathermap.org/data/2.5/weather?lat=-12.09&lon=-77.06&appid=76cd0cc1ffbcb7fcbc8193cf2c4758e5&units=imperial";
+
+async function apiFetch() {
+  try {
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data);
+      displayResults(data);
+    } else {
+      throw Error(await response.text());
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function displayResults(data) {
+  currentTemp.textContent = `${data.main.temp} °F`;
+  weatherIcon.src = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
+  captionDesc.textContent = data.weather[0].description;
+}
+
+apiFetch();
